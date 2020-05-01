29814469793d5aac61f75c6928a074a48f7e4fde
coonminer <coonminer@gmail.com>
2020-05-01 19:34:51 -0300

    create initial landing page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="header">
+        <h1>
+          Coon Miner
+        </h1>
+      </div>
+      <div className="container">
+        <form action="/" data-remote="true" method="post" className="search-container">
+          <input type="text" id="q" name="q" placeholder="What can I help you with today?" className="search-input"/>
+          <img className="search-icon" src="http://www.endlessicons.com/wp-content/uploads/2012/12/search-icon.png" alt="search icon"/>
+        </form>
+      </div>
+      <small>You are running this application in <b>{process.env.FOO}</b> mode.</small>
     </div>
   );
 }
